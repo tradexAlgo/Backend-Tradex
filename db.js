b7c04cc130e8618c54aef87ceb3286636a9a1b1e
tradexAlgo <tradexproooooo@gmail.com>
2024-12-24 01:25:11 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 mongoose
   .connect(
-    "mongodb+srv://alokmani9700:PmVEsJeG2Z3EY39X@cluster0.slz3e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://tradexproooooo:l37RQaOu7CUkbYd4@cluster0.slz3e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     // "mongodb+srv://manishsinghbais34766:Xmv02441!@vertexfx.wgtommg.mongodb.net/",
     {
       useNewUrlParser: true,
